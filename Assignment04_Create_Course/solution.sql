f592bc22bfe29b3328d5057501d9da74a1936736
1U25CA030-DIVYA <divyadharshini132879@gmail.com>
2026-09-08 18:49:27 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,9 +1,8 @@
-DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
-
--- Create Course table
-
--- Insert three records
-
--- Display structure
+CREATE TABLE Course (CourseID int(5) primary key,CourseName VARCHAR(30),Credits int(20),DepartmentID int(5));
+INSERT INTO Course VALUES (101, 'Python', 4, 101);
+INSERT INTO Course VALUES (102, 'Database Management', 4, 102);
+INSERT INTO Course VALUES (103, 'BCA', 3, 101);
+DESC Course;
+SELECT * FROM Course;
