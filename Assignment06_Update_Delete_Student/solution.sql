81ae38d426af29789e3e6d924f23aca459c3b518
1U25CA030-DIVYA <divyadharshini132879@gmail.com>
2026-09-08 22:04:29 +0530

    Update solution.sql

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -1,25 +1,18 @@
-DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
-
 CREATE TABLE Student(
     StudentID INT(5) PRIMARY KEY,
     StudentName VARCHAR(20),
     DOB DATE,
     Gender VARCHAR(10),
     DepartmentID INT(5),
-    Email VARCHAR(30),
-    PhoneNumber BIGINT
-);
-
+    Email VARCHAR(30));
 INSERT INTO Student(StudentID,StudentName,Gender,DepartmentID)
 VALUES
 (1001,'Arun','Male',101),
 (1002,'Divya','Female',102),
 (1003,'Karthik','Male',101);
-
--- Update Karthik's DepartmentID
-
--- Delete StudentID 1002
-
--- Display all records
+desc Student;
+update Student set departmentID =103 where Student name="Karthik";
+delete from Student where StudentID=1002;
+select * from Student;
